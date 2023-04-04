09b55e62662419d9a8e59326dc86f7b16f6d917a
Uğur KAYKAF <ugur.kaykaf@ogr.gelisim.edu.tr>
2023-04-04 17:56:42 +0300

    Create main.py

---
+++ dataVisualization_GuidedProject/main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+x=[0,2,4,6,8,10,10,14,16]
+y=[0,4,16,36,64,100,144,196,256]
+
+plt.plot(x,y)
+plt.title("Example data - Line plot")
+plt.show()
