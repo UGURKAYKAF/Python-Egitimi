163270529a0e3cd688818378131becd566cce670
Uğur KAYKAF <ugur.kaykaf@ogr.gelisim.edu.tr>
2023-04-04 17:59:19 +0300

    Create globalAIHub_finalExam.py

---
+++ turtle/globalAIHub_finalExam.py
@@ -0,0 +1,73 @@
+# name = "ugur"
+
+# def func():
+#     global name
+#     name = "ugur"
+
+# func()
+# print(name)
+
+# list ={
+#     "omer": 25,
+#     "winston":34,
+#     "jane":28,
+#     "robin":47
+# }
+
+# sum = 0
+
+# for i  in list.values():
+#     if i> 28:
+#         sum +=i
+
+# print(sum)
+
+
+# list = ['Laptop', 'Headset','Second Monitor', 'Usb Driver', 'External Driver']
+# list.append(['Mouse','Pencil','Pen'])
+# list.remove('Mouse')
+# list[0:3]
+
+# username = 'New York'
+# password = '2022'
+
+# input_username = input("Enter a username: ")
+# input_password = input("Enter a password: ")
+
+# if input_username == username and input_password == password:
+#     print("a")
+# elif input_username == username and input_password != password:
+#     print("b")
+# elif input_username != username and input_password == password:
+#     print("c")
+
+
+# list1 = [1,5,7,2,4,12]
+# result_list = []
+
+# def lets_add():
+#     for i in list1:
+#         if i%2 == 0:
+#             result_list.append(i)
+
+# lets_add()
+# print(result_list)
+
+
+# language = ["eng","span","turk","ıta","chi"]
+
+# def select_language(selection):
+#     if selection in language:
+#         print(f'You selected: {selection}.')
+#     else:
+#         print(f'{selection} is not available.')
+
+# selection =  input('Please, select a language: ')
+# select_language(selection)
+
+
+# x = 27.5
+# if(int(x)==x):
+#     print(f"{int(x)} is equal to {x}")
+# else:
+#     print(f"{int(x)} is not equal to {x}")
